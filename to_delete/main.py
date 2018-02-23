122dacb8a16aa0d0ab85a472e37451baea7375ec
jjousun <jjousun@gmail.com>
2018-02-23 11:54:21 -0800

    Cleaning up files

--- to_delete/main.py
+++
@@ -1,5 +0,0 @@
-from app import app, db
-
-
-# if __name__ == '__main__': # Erica separated this into a file called run.py
-#     app.run(debug=True) #remove this option for production
--- to_delete/views.py
+++
@@ -1,3 +0,0 @@
-from flask import Flask, request, render_template, jsonify
-from app import app
-from models import User
